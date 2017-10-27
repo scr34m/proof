62abfd6c1c4b390bfca81293fd8391739ebfd511
Gabor Gyorvari <scr34m@gmail.com>
2017-10-27 19:08:35 +0200

    Browser based notification remove

--- assets/js/worker.js
+++
@@ -1,9 +0,0 @@
-var interval;
-
-self.addEventListener('message', function (e) {
-    if (e.data == 'start') {
-        setTimeout(function () {
-            self.postMessage('tick');
-        }, 3000);
-    }
-}, false);
